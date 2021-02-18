82fb86b6ad340aa2bcecbeaad91cf76a59e858a1
Mark Chandler <mark@chanchan.io>
2021-02-17 19:05:11 -0500

    chore: configure environment

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  env: {
+    node: true,
+    "jest/globals": true,
+  },
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "jest",
+    "jest-dom",
+    "jsx-a11y",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jest/recommended",
+    "plugin:jest-dom/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+}
